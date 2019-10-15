0cfc4124ab84f919339d201f0cc45c10a9104f41
Dinne Bosman <dwjbosman@yahoo.com>
2019-10-15 17:10:09 +0200

    example project

---
+++ example_project/mytest.py
@@ -0,0 +1,10 @@
+def hallo():
+    pass
+
+
+def nogeen():
+    # rereireire rereieri rereiire   reireirei rereireire r reer e reerreireireireireirei rre  re reirreiiererere rereire
+    pass
+    x=10
+
+print("hello")
